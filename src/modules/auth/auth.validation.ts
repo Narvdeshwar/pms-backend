2a71d24d21c003b16ee58416272b82c769199a47
narvdeshwar <sam.07soni@gmail.com>
2026-01-25 17:31:46 +0530

    added auth Validation using the zod

---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,18 @@
+import { email, z } from "zod"
+
+// Registration Check
+export const RegisterSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(6),
+  role: z.enum(["ADMIN", "OPERATOR", "MANAGER", "VIEWER"]).optional()
+})
+
+// Login Check
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6)
+})
+
+export type RegisterInput = z.infer<typeof RegisterSchema>
+export type LoginInput = z.infer<typeof LoginSchema>
